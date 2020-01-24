61c7902aa99d6bdcb268d4f52a1f1c09f98bbb82
Yudistira Eka Putra <38902040+yudisputra@users.noreply.github.com>
2020-01-24 23:50:29 +0700

    Sudah Bisa Kirim Minus dari input dimasukkan string kok error ya

--- app/src/main/java/com/example/jacobmobile/model/PostSaran.java
+++ app/src/main/java/com/example/jacobmobile/model/PostSaran.java
@@ -2,30 +2,46 @@ package com.example.jacobmobile.model;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class PostSaran {
 
     @SerializedName("status")
-    String status;
+    private String status;
+
     @SerializedName("result")
-    Saran mSaran;
+    private List<Saran> mSaran;
+
     @SerializedName("message")
-    String message;
+    private String message;
+
+    public PostSaran(String status, List<Saran> mSaran, String message) {
+        this.status = status;
+        this.mSaran = mSaran;
+        this.message = message;
+    }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
+
+    public List<Saran> getmSaran() {
+        return mSaran;
+    }
+
+    public void setmSaran(List<Saran> mSaran) {
+        this.mSaran = mSaran;
+    }
+
     public String getMessage() {
         return message;
     }
+
     public void setMessage(String message) {
         this.message = message;
-    }
-    public Saran getSaran() {
-        return mSaran;
-    }
-    public void setKontak(Saran saran) {
-        mSaran = saran;
     }
 }
--- app/src/main/java/com/example/jacobmobile/rest/ApiInterface.java
+++ app/src/main/java/com/example/jacobmobile/rest/ApiInterface.java
@@ -2,16 +2,19 @@ package com.example.jacobmobile.rest;
 
 import com.example.jacobmobile.model.PostSaran;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiInterface {
 
+    @GET("saran")
+    Call<PostSaran> ambilsaran();
 
     @FormUrlEncoded
-    @POST("inputsaran")
-    Call<PostSaran> inputsaran(@Field("saran") String saran);
-
+    @POST("saran")
+    Call<PostSaran> inputsaran(@Field("saran") String saran, @Field("topik") String topik, @Field("urgensi") String urgensi, @Field("sentimen") String sentimen);
 }
